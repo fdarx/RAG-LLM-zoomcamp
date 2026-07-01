5c99d2a99f5ea2d60e94a18192b9899abb6943af
Fahad Dar <fahaddar228@gmail.com>
2026-07-02 02:30:13 +0500

    started the 01-agentic-rag

---
+++ setup/test_gemini_connection.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+from google import genai
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Say hi if you can read this"
+)
+print(response.text)
